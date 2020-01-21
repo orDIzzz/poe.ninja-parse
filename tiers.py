1de3b56f00972a39d3cae44c8d79ae7f25b76d20
Pavel Malyshev <paulinio09@gmail.com>
2020-01-21 16:08:42 +0300

    Refactoring bad code(added classes)

--- tiers.py
+++ tiers.py
@@ -1,60 +1,88 @@
 import json
-import pickle
-
-tier1 = 12
-tier2 = 5
 
 
-def tiers(parse_file, value_tier_1=12, value_tier_2=5):
-    tier = {'tier_1': [], 'tier_2': [], 'tier_3': []}
-    with open(parse_file, 'r', encoding='utf-8') as parse_file:
-        parsed = json.load(parse_file)
-        all_items = dict()
-        for p in parsed.keys():
-            for n in parsed[p]:
-                item_name = n['item_name']
-                price = float(n['price'])
-                if price > value_tier_1:
-                    tier['tier_1'].append(item_name)
-                elif value_tier_2 < price <= value_tier_1:
-                    tier['tier_2'].append(item_name)
-                else:
-                    tier['tier_3'].append(item_name)
-            all_items[p] = tier
-            tier = {'tier_1': [], 'tier_2': [], 'tier_3': []}
-        manage_categories(all_items)
-        print(all_items.keys())
-    return all_items
+class Tiers:
+    def __init__(self, groups, parse_file='parse.json',
+                 tier_1=None, tier_2=None, tier_3=None,
+                 tier_1_price=12, tier_2_price=5):
+        self.groups = groups
+        self.parse_file = parse_file
+        if tier_3 is None:
+            tier_3 = []
+        if tier_2 is None:
+            tier_2 = []
+        if tier_1 is None:
+            tier_1 = []
+        self.tier_1 = list(tier_1)
+        self.tier_2 = list(tier_2)
+        self.tier_3 = list(tier_3)
+        self.tier_1_price = abs(float(tier_1_price))
+        self.tier_2_price = abs(float(tier_2_price))
+        if self.tier_2_price > self.tier_1_price:
+            raise ValueError("Wrong tier prices. Tier 1 price must be more than Tier 2")
+
+    def take_bases(self):
+        with open(self.parse_file, 'r', encoding='utf-8') as parsed:
+            parsed = json.load(parsed)
+            self.manage_tiers(parsed, self.groups)
+        return self.tier_1, self.tier_2, self.tier_3
+
+    def manage_tiers(self, parsed, groups):
+        for n in parsed[groups]:
+            item_name = n['item_name']
+            price = float(n['price'])
+            if price > self.tier_1_price:
+                self.tier_1.append(item_name)
+            elif self.tier_2_price < price <= self.tier_1_price:
+                self.tier_2.append(item_name)
+            else:
+                self.tier_3.append(item_name)
+        return self.tier_1, self.tier_2, self.tier_3
+
+    def __repr__(self):
+        return f'<Group is {self.groups}>'
 
 
-def manage_categories(all_items):
-    unique_dict = {'Uniques': {'tier_1': [], 'tier_2': [], 'tier_3': []}}
-    uniques = 'Unique Jewels', 'Unique Flasks', 'Unique Weapons', 'Unique Armours', 'Unique Accessories'
-    for types in all_items.keys():
-        if types in uniques:
-            for p in all_items[types].pop('tier_1'):
-                unique_dict['Uniques']['tier_1'].append(p)
-            for p in all_items[types].pop('tier_2'):
-                unique_dict['Uniques']['tier_2'].append(p)
-            for p in all_items[types].pop('tier_3'):
-                unique_dict['Uniques']['tier_3'].append(p)
-    all_items.update(unique_dict)
-    all_items_copy = all_items.copy()
-    for k in all_items_copy.keys():
-        for i in uniques:
-            if i == k:
-                all_items.pop(i)
-    return all_items
+class Uniques(Tiers):
+    def __init__(self, groups, parse_file='parse.json',
+                 tier_1=None, tier_2=None, tier_3=None,
+                 tier_1_price=12, tier_2_price=5,
+                 unique_types=('Unique Jewels', 'Unique Flasks', 'Unique Weapons',
+                               'Unique Armours', 'Unique Accessories')):
+        super().__init__(groups, parse_file, tier_1, tier_2, tier_3, tier_1_price, tier_2_price)
+        self.unique_types = unique_types
+
+    def take_bases(self):
+        with open(self.parse_file, 'r', encoding='utf-8') as parsed:
+            parsed = json.load(parsed)
+            for groups in self.unique_types:
+                if groups in parsed.keys():
+                    self.manage_tiers(parsed, groups)
+        return self.tier_1, self.tier_2, self.tier_3
 
 
-def save_tiers(parser):
-    with open('tiers.csv', 'w', encoding='utf-8') as sorted_items:
-        sorted_items.write(str(tiers(parser, tier1, tier2)))
+fragments = Tiers('Fragments')
+print(fragments.take_bases()[0])
 
-    with open('tiers.pickle', 'wb') as sorted_items2:
-        pickle.dump(tiers(parser, tier1, tier2), sorted_items2)
+oils = Tiers('Oils')
 
+incubators = Tiers('Incubators')
 
-save_tiers('parse.json')
+scarabs = Tiers('Scarabs')
 
-# unique = 'Unique Jewels', 'Unique Flasks', 'Unique Weapons', 'Unique Armours', 'Unique Accessories'
+fossils = Tiers('Fossils')
+
+resonators = Tiers('Resonators')
+
+essences = Tiers('Essences')
+
+div_cards = Tiers('Divination Cards')
+
+prophecies = Tiers('Prophecies')
+
+uni_maps = Tiers('Unique Maps')
+
+beasts = Tiers('Beasts')
+
+uniques = Uniques('Uniques')
+print(uniques.take_bases()[0])
