a22b87b7912125e65827f22dc0496bf2cb56be73
Pavel Malyshev <paulinio09@gmail.com>
2020-01-22 11:04:46 +0300

    added function to check and delete excess items

--- tiers.py
+++ tiers.py
@@ -2,10 +2,12 @@ import json
 
 
 class Tiers:
-    def __init__(self, groups, parse_file='parse.json',
+    def __init__(self, contents, parse_file='parse.json',
                  tier_1=None, tier_2=None, tier_3=None,
-                 tier_1_price=12, tier_2_price=5):
-        self.groups = groups
+                 tier_1_price=12, tier_2_price=5,
+                 excess=('Timeworn Reliquary Key', 'Ancient Reliquary Key', 'Tabula Rasa')):
+        self.excess = excess
+        self.contents = contents
         self.parse_file = parse_file
         if tier_3 is None:
             tier_3 = []
@@ -24,45 +26,51 @@ class Tiers:
     def take_bases(self):
         with open(self.parse_file, 'r', encoding='utf-8') as parsed:
             parsed = json.load(parsed)
-            self.manage_tiers(parsed, self.groups)
+            self.manage_tiers(parsed, self.contents)
         return self.tier_1, self.tier_2, self.tier_3
 
-    def manage_tiers(self, parsed, groups):
-        for n in parsed[groups]:
+    def manage_tiers(self, parsed, contents):
+        for n in parsed[contents]:
             item_name = n['item_name']
             price = float(n['price'])
-            if price > self.tier_1_price:
-                self.tier_1.append(item_name)
-            elif self.tier_2_price < price <= self.tier_1_price:
-                self.tier_2.append(item_name)
-            else:
-                self.tier_3.append(item_name)
+            if self.remove_excess(item_name) is not None:
+                if price > self.tier_1_price:
+                    self.tier_1.append(item_name)
+                elif self.tier_2_price < price <= self.tier_1_price:
+                    self.tier_2.append(item_name)
+                else:
+                    self.tier_3.append(item_name)
         return self.tier_1, self.tier_2, self.tier_3
 
+    def remove_excess(self, item_name):
+        if item_name in self.excess:
+            return None
+        else:
+            return item_name
+
     def __repr__(self):
-        return f'<Group is {self.groups}>'
+        return f'<Group is {self.contents}>'
 
 
 class Uniques(Tiers):
-    def __init__(self, groups, parse_file='parse.json',
+    def __init__(self, contents, parse_file='parse.json',
                  tier_1=None, tier_2=None, tier_3=None,
                  tier_1_price=12, tier_2_price=5,
                  unique_types=('Unique Jewels', 'Unique Flasks', 'Unique Weapons',
                                'Unique Armours', 'Unique Accessories')):
-        super().__init__(groups, parse_file, tier_1, tier_2, tier_3, tier_1_price, tier_2_price)
+        super().__init__(contents, parse_file, tier_1, tier_2, tier_3, tier_1_price, tier_2_price)
         self.unique_types = unique_types
 
     def take_bases(self):
         with open(self.parse_file, 'r', encoding='utf-8') as parsed:
             parsed = json.load(parsed)
-            for groups in self.unique_types:
-                if groups in parsed.keys():
-                    self.manage_tiers(parsed, groups)
+            for contents in self.unique_types:
+                if contents in parsed.keys():
+                    self.manage_tiers(parsed, contents)
         return self.tier_1, self.tier_2, self.tier_3
 
 
 fragments = Tiers('Fragments')
-print(fragments.take_bases()[0])
 
 oils = Tiers('Oils')
 
@@ -70,19 +78,13 @@ incubators = Tiers('Incubators')
 
 scarabs = Tiers('Scarabs')
 
-fossils = Tiers('Fossils')
-
 resonators = Tiers('Resonators')
 
-essences = Tiers('Essences')
+fossils = Tiers('Fossils')
 
 div_cards = Tiers('Divination Cards')
 
-prophecies = Tiers('Prophecies')
-
-uni_maps = Tiers('Unique Maps')
-
-beasts = Tiers('Beasts')
+uni_maps = Tiers('Unique Maps')  # has only 2 tiers
 
 uniques = Uniques('Uniques')
-print(uniques.take_bases()[0])
+# print(uniques.take_bases()[0])
